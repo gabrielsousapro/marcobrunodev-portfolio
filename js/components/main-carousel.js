65793171170dd6a7637822417705c749c65587f8
gabrielsousapro <sousadev.pro@gmail.com>
2021-04-21 11:48:49 -0300

    add js funcionality to both buttons on carousel

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,8 +1,14 @@
 const btnRight = document.querySelector('.btn-arrow.-right');
+const btnLeft = document.querySelector('.btn-arrow.-left')
 const elements = document.querySelector('.elements');
-let pixels = 10;
+let pixels = 100;
 
 btnRight.addEventListener('click', function() {
-    pixels = pixels + 20;
-    elements.style = `transform: translateX(${pixels})`;
-})
+    pixels = pixels + 100;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+
+btnLeft.addEventListener('click', function() {
+    pixels = pixels - 100;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
